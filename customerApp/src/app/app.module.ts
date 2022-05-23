fb9a5f4b0da5a21a941c567562b5b7f4efa7a6fe
sathwik <sathwik.m@mpokkte.com>
2022-05-23 23:50:48 +0530

    angular update Employee ui and integration done

--- customerApp/src/app/app.module.ts
+++ customerApp/src/app/app.module.ts
@@ -8,16 +8,19 @@ import { EmployeesComponent } from './components/employees/employees.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
 
 const appRoutes: Routes = [
-  { path: '', component:EmployeesComponent}
+  { path: '', component:EmployeesComponent},
+  { path: 'edit/:id', component:EmployeeEditComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent, 
-    NavbarComponent
+    NavbarComponent,
+    EmployeeEditComponent
   ],
   imports: [
     BrowserModule,
--- customerApp/src/app/service/data.service.ts
+++ customerApp/src/app/service/data.service.ts
@@ -18,4 +18,12 @@ export class DataService {
   deleteData(id:any){
     return this.httpClient.delete('http://127.0.0.1:8000/api/deleteEmployee/'+id);
   }
+
+  getEmployeeById(id:any){
+    return this.httpClient.get('http://127.0.0.1:8000/api/employee/'+id);
+  }
+
+  updateData(id:any, data:any){
+    return this.httpClient.put('http://127.0.0.1:8000/api/updateEmployee/'+id, data);
+  }
 }

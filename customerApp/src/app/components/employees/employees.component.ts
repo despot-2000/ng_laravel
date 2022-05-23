aa5b48f76d3290d4f559dee980b3c3b04daa0745
sathwik <sathwik.m@mpokkte.com>
2022-05-23 22:38:09 +0530

    angular addEmployeeui and integratio done

---
+++ customerApp/src/app/components/employees/employees.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/service/data.service';
+import { Employee } from 'src/app/employee';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+  employees:any;
+  employee = new Employee();
+  constructor(private dataService:DataService) { }
+
+  ngOnInit(): void {
+    this.getEmployeesData();
+  }
+
+  getEmployeesData(){
+    this.dataService.getData().subscribe(res => {
+      this.employees = res;
+    });
+  }
+
+  insertData(){
+    this.dataService.insertData(this.employee).subscribe(res => {
+      this.getEmployeesData();
+    });
+  }
+}
